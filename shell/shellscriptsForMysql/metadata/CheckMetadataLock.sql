fcc73f9d42d2899535007445e2b5fec58cd5e931
魏亚萍 <weiyaping@jiagouyun.com>
2017-08-21 16:25:00 +0800

    add metadata lock

--- shell/shellscriptsForMysql/metadata/CheckMetadataLock.sql
+++ shell/shellscriptsForMysql/metadata/CheckMetadataLock.sql
@@ -1,16 +1,3 @@
-```shell
-# 查看当前的线程情况
-show processlist;
-
-# 查看当前线程中非系统用户且查询命令为select的线程id和执行时间
-select id,time from information_schema.processlist where USER !='system user' and COMMAND='query';
-
-# 查看当前线程中是innodb事务的线程情况
-select * from information_schema.processlist where id in (select trx_mysql_thread_id from information_schema.innodb_trx)\G;
-
-# 查看当前线程中存在metadata锁的线程id和执行时间
-select id,time from information_schema.processlist where state='Waiting for table metadata lock';
-
 # 查看有metalock锁的线程
 # 查看未提交的事务运行时间，线程id，用户等信息
 # 查看未提交的事务运行时间，线程id，用户，sql语句等信息
@@ -25,7 +12,3 @@ select * from performance_schema.threads where thread_id=46052\G;
 select * from information_schema.processlist where id=xxx\G;
 
 select processlist_id from performance_schema.threads where thread_id==(select thread_id from performance_schema.events_statements_current where SQL_TEXT like '%booboo%');
-```
-
-
-
